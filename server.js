2c73c24f01487ac7a14851d0228d8d01509f5bfa
dinopascale <dino.pascale@gmail.com>
2018-12-12 12:12:09 +0100

    history fallback heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const serveStatic = require('serve-static');
+// const serveStatic = require('serve-static');
 const path = require('path');
+
 var history = require('connect-history-api-fallback');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+// app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(history());
+app.use(express.static(__dirname));
 
 const port = process.env.PORT || 80;
 
